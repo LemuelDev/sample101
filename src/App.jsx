f23592e069db8a6f7d4cb8b5167d9a89beb6404f
LemuelDev <lemueldev2023@gmail.com>
2024-02-03 17:33:07 +0800

    hay payna

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Approutes from "./Routes"
 import Navbar from "./components/Navbar"
 import './App.css'
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 function App() {
   return (
     <main>
-       <Router basename="/sample101/">
+       <Router >
        <Navbar/>
         <section>
           <Approutes/>
